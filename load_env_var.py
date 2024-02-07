5d669f0ea92ce2b130fde6e71e9bf549f9313fb2
Ana Gomez <anitaggrr@gmail.com>
2024-02-07 12:55:47 +0100

    feat: functions to retrieve the env variables

--- load_env_var.py
+++ load_env_var.py
@@ -15,11 +15,26 @@ def load_env_variables_db():
     db_driver = os.getenv('DB_DRIVER')
     return db_server, db_name, db_user, db_pass, db_port, db_driver
 
+
 # AzureOpenAI environment variables
 def load_env_variables_openai():
     openai.api_type = "azure"
-    openai.api_base = os.getenv("AZURE_OPENAI_API_BASE")
+    openai.api_base = os.getenv('AZURE_OPENAI_API_BASE')
     openai.api_version = os.getenv('AZURE_API_VERSION')
-    openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
-    return openai.api_base, openai.api_version, openai.api_key
+    openai.api_key = os.getenv('AZURE_OPENAI_API_KEY')
+    llm_model = os.getenv('AZURE_OPENAI_LLM_MODEL')
+    return openai.api_type, openai.api_base, openai.api_version, openai.api_key, llm_model
 
+
+# AzureSearch environment variables
+def load_env_variables_azure_search():
+    search_endpoint = os.getenv('SEARCH_ENDPOINT')
+    search_key = os.getenv('SEARCH_ADMIN_KEY')
+    search_index_name = os.getenv('SEARCH_INDEX_NAME')
+    return search_endpoint, search_key, search_index_name
+
+
+# AzureSpeech environment variables
+def load_env_variables_azure_speech():
+    speech_api_key = os.getenv('SPEECH_API_KEY')
+    return speech_api_key
